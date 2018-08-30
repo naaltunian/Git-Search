21bd2c270ba8ebfb25d8d8bf682035022e2e74cc
naaltunian <naaltunian@icloud.com>
2018-08-30 15:35:37 -0400

    create title component

--- src/App.js
+++ src/App.js
@@ -2,34 +2,50 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import UserForm from './components/UserForm.js';
+import Title from "./components/Title.js";
 
 // https://api.github.com/users/{user}
 
 
 class App extends Component {
   state = {
+    name: null,
     repos: null,
-
+    bio: null,
+    blog: null,
+    company: null,
+    email: null,
+    followers: null,
+    following: null,
+    html_url: null
   }
 
   getUser = (e) => {
     e.preventDefault();
     const user = e.target.elements.username.value;
-    axios.get(`https://api.github.com/users/${user}`)
-    .then((res) => {
-      const repos = res.data.public_repos;
-      this.setState({
-        repos: repos
+    if(user){
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data;
+        this.setState({
+          repos: repos.public_repos
+        });
+      }).catch((error) => {
+        this.setState({
+          repos: "user not found"
+        });
       });
-    });
+    } else {
+      return;
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>HTTP Calls in React</h1>
+        <Title />
         <UserForm userInfo={this.getUser} />
-        {this.state.repos ? <p>Number of repos: {this.state.repos}</p>}
+        { this.state.repos ? <p>Number of repos: {this.state.repos}</p> : <p>Enter username</p> }
       </div>
     );
   }
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = (props) => {
+  return(
+    { this.state.repos ? <p>Number of repos: {this.state.repos}</p> : <p>Enter sleep</p> }
+  );
+}
+
+export default Display;
